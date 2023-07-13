b90400e319ac4112d90c9d79a6c0f851e32346cb
Brianclikclak <brianclikclak@hotmail.com>
2023-07-13 20:59:56 +0200

    keep testing

--- src/main/java/org/example/war/War.java
+++ src/main/java/org/example/war/War.java
@@ -2,10 +2,12 @@ package org.example.war;
 
 import org.example.vikins.Vikings;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class War {
+
     private List<Vikings> vikingArmy;
     private List<Object>  saxonArmy;
 
@@ -15,9 +17,28 @@ public class War {
     }
 
     public List<Vikings> getVikingArmy() {
+
         return vikingArmy;
     }
     public List<Object> getSaxonArmy(){
+
         return saxonArmy;
     }
+
+    public void addViking(Vikings viking ) {
+        vikingArmy.add(viking);
+
+    }
+    public void addSaxon(Object saxon) {
+        saxonArmy.add(saxon);
+    }
+
+    public void vikingAttack(){
+         viking.strength = 300;
+         saxon.health -= viking.strength;
+         
+    }
+
+
 }
+
